643a21b9c38de70311e04ed5d7c43836381e1e38
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-10-26 12:30:14 -0700

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -5,7 +5,8 @@ echo "Syncing dependencies ..."
 mkdir "data"
 
 PIDS=""
-./sync.sh https://github.com/skyhuppa/android_kernel_xiaomi_land.git "data/kernel" "${REF}" &
+# ./sync.sh https://github.com/skyhuppa/android_kernel_xiaomi_land.git "data/kernel" "${REF}" &
+./sync.sh https://github.com/vantoman/kernel_xiaomi_sm6150.git "data/kernel" "${REF}" &
 PIDS="${PIDS} $!"
 # ./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
 ./sync.sh https://github.com/skyhuppa/gcc.git "data/gcc" &
