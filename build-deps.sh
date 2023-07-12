5c846a1d7b20002b1d1fbf9c112d312ee3b04aa3
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-12 13:13:19 -0700

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -7,10 +7,10 @@ mkdir "data"
 PIDS=""
 # ./sync.sh https://github.com/skyhuppa/kernel_land.git "data/kernel" "${REF}" &
 #./sync.sh https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git "data/kernel" "${REF}" &
-./sync.sh https://github.com/skyhuppa/android_kernel_motorola_msm8937-2.git "data/gcc" &
+./sync.sh https://github.com/skyhuppa/android_kernel_motorola_msm8937-2.git "data/kernel" "${REF}" &
 PIDS="${PIDS} $!"
-./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
-# ./sync.sh https://github.com/skyhuppa/gcc.git "data/gcc" &
+# ./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
+./sync.sh https://github.com/skyhuppa/gcc.git "data/gcc" &
 PIDS="${PIDS} $!"
 ./sync.sh https://github.com/skyhuppa/proton-clang.git "data/clang" &
 PIDS="${PIDS} $!"
