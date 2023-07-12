1177e9862c88c40fb82122775eec0e90456c66c0
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-12 08:04:28 -0700

    Update build-deps.sh

--- build-deps.sh
+++ build-deps.sh
@@ -6,8 +6,7 @@ mkdir "data"
 
 PIDS=""
 # ./sync.sh https://github.com/skyhuppa/kernel_land.git "data/kernel" "${REF}" &
-#./sync.sh https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git "data/kernel" "${REF}" &
-./sync.sh https://github.com/android-linux-stable/msm-3.18.git "data/kernel" "${REF}" &
+./sync.sh https://github.com/skyhuppa/Kernel_Infinix_HotS3_X573_TestBuild.git "data/kernel" "${REF}" &
 PIDS="${PIDS} $!"
 ./sync.sh https://github.com/skyhuppa/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git "data/gcc" &
 # ./sync.sh https://github.com/skyhuppa/gcc.git "data/gcc" &
