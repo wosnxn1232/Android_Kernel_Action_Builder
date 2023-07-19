dd9ed29aaf8ceb960668de47a22c1d011546d7a8
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-19 15:07:59 -0700

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,37 +1,6 @@
 #!/bin/bash
 
-# A Function to Send Posts to Telegram
-telegram_message() {
-	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
-	-d chat_id="${TG_CHAT_ID}" \
-	-d parse_mode="HTML" \
-	-d text="$1"
-}
-
 for file in *.zip; do
     echo "Uploading ${file} ..."
     curl --upload-file ${file} https://transfer.sh/${file}
-
-# Send the Message on Telegram
-echo -e \
-"
-![ Infinix-X573 ](https://skyhuppa.files.wordpress.com/2023/07/infinix-x573.jpg?w=984)
-📋 Kernel Builder
-
-✅ Build Completed Successfully!
-
-🔥 Build-CI: Github Runner
-📱 Device: Infinix-X573
-🖥 Kernel Verssion: 3.18.X
-⬇️ Download: <a href=\"${https://oshi.at/_*}\">Here</a>
-📅 Date: "$(date +%d\ %B\ %Y)"
-⏱ Time: "$(date +%T)"
-" > tg.html
-
-TG_TEXT=$(< tg.html)
-
-telegram_message "$TG_TEXT"
-
-echo " "
-    
 done
