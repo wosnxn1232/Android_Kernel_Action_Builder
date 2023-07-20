71354a1bcad69e18115635dad23d49c5c8ace167
Adasi Kaakyire <skyhuppa@hotmail.co.uk>
2023-07-20 21:41:35 +0000

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,6 +2,5 @@
 
 for file in *.zip; do
     echo "Uploading ${file} ..."
-   # curl --upload-file ${file} https://transfer.sh/${file}
-   curl --upload-file ${file} https://fromsmash.com/${file}
+    curl --upload-file ${file} https://transfer.sh/${file}
 done
